b8339086c89d9216efe5c1afc059256978abeb4f
Jae-hyeon Park <jhyeon@gmail.com>
2020-02-19 18:16:28 +0900

    Trim away unneeded comments

--- madanalysis5/tools/PAD/Build/SampleAnalyzer/User/Analyzer/ATLAS_SUSY_2018_04.cpp
+++ madanalysis5/tools/PAD/Build/SampleAnalyzer/User/Analyzer/ATLAS_SUSY_2018_04.cpp
@@ -279,12 +279,10 @@ bool ATLAS_SUSY_2018_04::Execute(SampleFormat& sample, const EventFormat& event)
   //// SRlow cut-3 : 2 tight taus (OS). //// 
   //double tight_low=myWeight*0.714815;
   //Manager()->SetCurrentEventWeight(tight_low);
-//  bool tight1 = (rand() % 100)/100. < sqrt(1565./2228.0); // 0.714815;
-//  bool tight2 = (rand() % 100)/100. < sqrt(1565./2228.0); // 0.714815;
   // 2228 and 1565 are Nraw before and after 2-tight-tau cut in SR-lowMass at
   // https://atlas.web.cern.ch/Atlas/GROUPS/PHYSICS/PAPERS/SUSY-2018-04/tabaux_01.pdf
-  bool tight1 = double(rand())/RAND_MAX < sqrt(1565./2228.0); // 0.714815;
-  bool tight2 = double(rand())/RAND_MAX < sqrt(1565./2228.0); // 0.714815;
+  bool tight1 = double(rand())/RAND_MAX < sqrt(1565./2228.0);
+  bool tight2 = double(rand())/RAND_MAX < sqrt(1565./2228.0);
   // bool two_tight_ratio = (rand() % 100)/100. < 0.7;
 
   if( !Manager()->ApplyCut(tight1 && tight2, "2 tight $\\tau$ (OS)") ) return true;
